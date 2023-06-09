89967984578fa04ff6e9ef6c352fcea0920933cc
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-09 20:33:43 +0600

    loading spinner added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
+import SpinnerLoader from "../components/SpinnerLoader/SpinnerLoader";
 
 const MainLayout = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <NavBar> </NavBar>
+             {/* ------Loading Spinner------ */}
+           {navigation.state === 'loading' && <SpinnerLoader> </SpinnerLoader>}
             <Outlet> </Outlet>
             <Footer> </Footer>
         </div>
