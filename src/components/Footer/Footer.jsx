55be659076950c272399508916678b62bd9f932f
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-10 21:13:42 +0600

    footer stracture added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,45 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h2 className="text-3xl font-bold text-center">This is Footer</h2>
+        <div className="pt-20 pb-10">
+            <div className="py-16 bg-[#0D0D0D]">
+
+                <div className=" container mx-auto ">
+                    <div className=" w-11/12 mx-auto grid  grid-cols-1 md:grid-cols-4 gap-4">
+
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, libero.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+          </div>
+           <div className="text-center mt-10">
+           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, corporis.</p>
+           </div>
         </div>
     );
 };
