5f984333b51899378086a8932b03cdf23ccb2459
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-12 16:59:48 +0600

    second slider complete

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import useTitle from "../../../hooks/useTitle";
 import TopSlider from "../TopSlider/TopSlider";
 import { ScrollRestoration } from "react-router-dom";
 import dancer1 from "../../../assets/images/homeSlider-img/dancer-1.png";
+import dancer2 from "../../../assets/images/homeSlider-img/dancer-2.png";
 import { FiArrowRight } from "react-icons/fi";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 const Home = () => {
@@ -66,6 +67,45 @@ const Home = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* -----------Second Banner------------ */}
+      <div className=" pb-5 md:py-10 bg-[#9171558c] dark:bg-[#917155] ">
+        <div className="container mx-auto">
+          <div className="flex flex-col-reverse md:flex-row items-start px-5 ">
+            
+            <div className="md:w-7/12 mt-5 md:mt-20">
+              <h1 className="text-4xl lg:text-5xl font-bold text-[#17161D] ">
+                Fulfil Your Dream with us
+              </h1>
+              <p className="text-gray-900 dark:text-[#dfdee0] w-full md:w-8/12 lg:w-7/12 font-[500] md:my-8">
+              Start your first journey with us. We are always ready to welcome you. We have the ability to make you great. We have the best dancing Instructors/ Guru all over the world. We can Learn all kinds of forms of dance here.
+              </p>
+
+              <div className=" mt-3 md:mt-10 flex items-center gap-5">
+                {" "}
+                <p
+                  style={{ letterSpacing: "2px" }}
+                  className="uppercase font-bold text-[#17161D] dark:text-[#dfdee0] "
+                >
+                  Learn More
+                </p>{" "}
+                <span className="p-2 border-[#17161D] dark:border-[#dfdee0] rounded-full border-2">
+                  {" "}
+                  <FiArrowRight className="text-[#17161D] dark:text-[#dfdee0]" />{" "}
+                </span>
+              </div>
+
+              
+            </div>
+
+            <div className=" md:w-5/12 ">
+              <img src={dancer2} className=" w-10/12 mx-auto mt-5 md:mt-0" alt="" />
+            </div>
+
           </div>
         </div>
       </div>
