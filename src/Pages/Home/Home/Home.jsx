905ce70fb15aa6fc02400a0305e222e5fbb84136
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-13 20:48:52 +0600

    mail sending section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import useTitle from "../../../hooks/useTitle";
 import PopularClasses from "../PopularClasses/PopularClasses";
+import SendMail from "../PopularClasses/SendMail/SendMail";
 import PopularInstructors from "../PopularInstructors/PopularInstructors";
 import TopSlider from "../TopSlider/TopSlider";
 import { ScrollRestoration } from "react-router-dom";
@@ -10,14 +11,17 @@ const Home = () => {
 
   return (
     <div>
-      {/* -----------Top Banner -------------- */}
+      {/* -----------Top Banner --------------------- */}
        <TopSlider> </TopSlider>
 
-       {/* --------Popular Classes section--------- */}
+       {/* --------Popular Classes section-------------*/}
        <PopularClasses> </PopularClasses>
 
        {/* --------Popular Instructors section--------- */}
        <PopularInstructors> </PopularInstructors>
+
+       {/* --------Send Massage section (Extra)--------- */}
+        <SendMail> </SendMail>
 
       <ScrollRestoration />
     </div>
