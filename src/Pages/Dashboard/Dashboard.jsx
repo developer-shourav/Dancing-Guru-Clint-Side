1ec90c43050e3251f820ac99707c4cfb673be8f2
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-14 11:00:55 +0600

    user role based content randering complete

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -8,20 +8,33 @@ const Dashboard = () => {
     /* ---------Taking user form DB and firebase----- */
     const {user} = useContext(AuthContext);
 
-    const [dbUser, setDbUser] = useState(null);
+    /* -------------Define The user Role----------- */
+    const [userRole, setUserRole] = useState(null);
+
 
     useEffect(() => {
         fetch(`https://dancing-guru-server.vercel.app/users/${user?.email}`)
         .then( res => res.json())
-        .then( data => setDbUser(data))
+        .then( data => {
+            if(user?.email === data.userEmail){
+                setUserRole(data?.role)
+            }
+        })
     }, [user])
 
-    if(user?.email === dbUser?.userEmail){
-        console.log(dbUser?.role);
-    }
+
     return (
-        <div>
-            <h2 className="text-2xl">Dashboard  Page</h2>
+        <div className="container mx-auto">
+            {
+                userRole === 'admin' && <h2 className="text-2xl"> Welcome Admin </h2>
+            }
+            {
+                userRole === 'instructor' && <h2 className="text-2xl"> Welcome instructor </h2>
+            }
+            {
+                userRole === 'user' && <h2 className="text-2xl"> Welcome User </h2>
+            }
+            
         </div>
     );
 };
