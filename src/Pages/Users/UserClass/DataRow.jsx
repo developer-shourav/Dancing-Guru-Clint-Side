b743dc276e3fe85e8388e28df42006b813f62641
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-15 16:47:08 +0600

    user delete functionality complete

---
+++ src/Pages/Users/UserClass/DataRow.jsx
@@ -0,0 +1,61 @@
+import Swal from "sweetalert2";
+
+
+const DataRow = ({item, setReloadClass, reloadClass} ) => {
+     const {_id, classImage, className, instructorName} = item;
+     const handleDelete = () => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "Want to Delete this Class?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#d33",
+          cancelButtonColor: "#007700",
+          confirmButtonText: "Yes, Delete",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            
+            fetch(`https://dancing-guru-server.vercel.app/selectedCls/${_id}`, {
+              method:'DELETE'
+            })
+            .then( res => res.json())
+            .then (data => {
+              if(data.deletedCount > 0){
+                setReloadClass(!reloadClass)
+                Swal.fire({
+                  title: "Done !",
+                  text: "Class Delete successful",
+                  icon: "success",
+                });
+              }
+            })
+            
+          }
+        });
+      
+       }
+    return (
+        <tr>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-20 h-20 md:w-24 md:h-24">
+                <img src={classImage} alt="Class image" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{className}</div>
+            </div>
+          </div>
+        </td>
+        <td>{instructorName}</td>
+        
+        <td><button onClick={handleDelete}  className="btn bg-red-500 hover:bg-red-600">Delete</button></td>
+        <th>
+         <button className="btn btn-primary" > Pay</button>
+        </th>
+      </tr>
+    );
+};
+
+export default DataRow;
--- src/Pages/Users/UserClass/UserClass.jsx
+++ src/Pages/Users/UserClass/UserClass.jsx
@@ -1,8 +1,50 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../providers/AuthProviders";
+import DataRow from "./DataRow";
 
 const UserClass = () => {
+     const [selectedClass , setSelectedClass] = useState([]);
+     const {user} = useContext(AuthContext);
+     const [reloadClass, setReloadClass] = useState(true)
+ 
+     useEffect(() => {
+         fetch(`https://dancing-guru-server.vercel.app/selectedCls/${user?.email}`)
+         .then( res => res.json())
+         .then( data => setSelectedClass(data))
+     } , [reloadClass]);
+
+
     return (
-        <div>
-           <h2>This is all Classes of Student</h2> 
+        <div className=" w-full h-full  mx-auto ">
+           <h2 className="font-bold text-2xl text-center mb-10 mt-5">My Selected Class</h2> 
+           
+           <div className="overflow-x-auto w-full  px-5">
+        <table className="table w-full ">
+          {/* head */}
+          <thead className="font-extrabold ">
+            <tr className="">
+             
+              <th className=" text-[16px]">Class Name & Image</th>
+              <th className=" text-[16px]">Instructor</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {/* --------show data -------------- */}
+           {
+            selectedClass?.map( item  => <DataRow
+            key={item._id} item={item} setReloadClass={setReloadClass} reloadClass={reloadClass}
+            > 
+            </DataRow>)
+           }
+            
+          </tbody>
+         
+        </table>
+      </div>
+
         </div>
     );
 };
