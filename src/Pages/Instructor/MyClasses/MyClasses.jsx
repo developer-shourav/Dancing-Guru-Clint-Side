e46452caa74d6e2dd488618274e9eb0c400497ab
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-16 00:39:42 +0600

    instructors all class added

--- src/Pages/Instructor/MyClasses/MyClasses.jsx
+++ src/Pages/Instructor/MyClasses/MyClasses.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
+import MyClassesRow from "./MyClassesRow";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProviders";
 
 const MyClasses = () => {
-    return (
-        <div>
-            <h2>This is my Class</h2>
+     const {user} = useContext(AuthContext);
+     const [instClasses, setInstClasses] = useState([]);
+
+     useEffect(()=> {
+        fetch(`https://dancing-guru-server.vercel.app/classes/${user?.email}`)
+        .then( res => res.json())
+        .then( data => setInstClasses(data))
+     } ,[])
+
+     console.log(instClasses);
+     
+    return ( 
+        <div className=" w-full h-full  mx-auto ">
+           <h2 className="font-bold text-2xl text-center mb-10 mt-5">My Classes</h2> 
+           
+           <div className="overflow-x-auto w-full  px-5">
+        <table className="table w-full ">
+          {/* head */}
+          <thead className="font-extrabold ">
+            <tr className="">
+             
+              <th className=" text-[16px]">Class Name & Image</th>
+              <th className=" text-[16px]">Instructor</th>
+              <th className=" text-[16px]">Price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {/* --------show data -------------- */}
+           {
+            instClasses?.map( item  => <MyClassesRow
+            key={item._id} item={item} 
+            > 
+            </MyClassesRow>)
+           }
+            
+          </tbody>
+         
+        </table>
+      </div>
+
         </div>
     );
 };
