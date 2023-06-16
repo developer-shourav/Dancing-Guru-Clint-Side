929fa77ff65c66437bc8a4752ed2bec94ae323f3
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-16 11:54:44 +0600

    empty field message added

--- src/Pages/Users/EnrolledClass/EnrolledClass.jsx
+++ src/Pages/Users/EnrolledClass/EnrolledClass.jsx
@@ -16,33 +16,40 @@ const EnrolledClass = () => {
         <div className=" w-full h-full  mx-auto ">
            <h2 className="font-bold text-2xl text-center mb-10 mt-5">My Enrolled Class</h2> 
            
-           <div className="overflow-x-auto w-full  px-5">
-        <table className="table w-full ">
-          {/* head */}
-          <thead className="font-extrabold ">
-            <tr className="">
-             
-              <th className=" text-[16px]">Class Name & Image</th>
-              <th className=" text-[16px]">Instructor</th>
-              <th className=" text-[16px]">Price</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-
-            {/* --------show data -------------- */}
-           {
-            enrolleddClass?.map( item  => <EnrolledRow
-            key={item._id} item={item} 
-            > 
-            </EnrolledRow>)
-           }
-            
-          </tbody>
-         
-        </table>
-      </div>
+        {
+          enrolleddClass.length >= 1 &&    <div className="overflow-x-auto w-full  px-5">
+          <table className="table w-full ">
+            {/* head */}
+            <thead className="font-extrabold ">
+              <tr className="">
+               
+                <th className=" text-[16px]">Class Name & Image</th>
+                <th className=" text-[16px]">Instructor</th>
+                <th className=" text-[16px]">Price</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+  
+              {/* --------show data -------------- */}
+             {
+              enrolleddClass?.map( item  => <EnrolledRow
+              key={item._id} item={item} 
+              > 
+              </EnrolledRow>)
+             }
+              
+            </tbody>
+           
+          </table>
+        </div>
+        }
+        {
+           enrolleddClass.length == 0 && <div className="text-center text-gray-400 bg-stone-200 py-20 w-10/12 lg:w-8/12 mx-auto rounded-3xl  font-bold"> <h2 className=" text-2xl lg:text-4xl">No Class has Enrolled Yet</h2>
+           <p className="font-normal mt-2 text-gray-600">Please make payment after selecting the class for Enrolment </p>
+           </div>
+        }
 
         </div>
     );
--- src/Pages/Users/UserClass/UserClass.jsx
+++ src/Pages/Users/UserClass/UserClass.jsx
@@ -18,7 +18,7 @@ const UserClass = () => {
         <div className=" w-full h-full  mx-auto ">
            <h2 className="font-bold text-2xl text-center mb-10 mt-5">My Selected Class</h2> 
            
-           <div className="overflow-x-auto w-full  px-5">
+         { selectedClass.length >= 1 &&  <div className="overflow-x-auto w-full  px-5">
         <table className="table w-full ">
           {/* head */}
           <thead className="font-extrabold ">
@@ -43,7 +43,15 @@ const UserClass = () => {
           </tbody>
          
         </table>
-      </div>
+         </div>
+
+         }
+
+       {
+           selectedClass.length == 0 && <div className="text-center text-gray-400 bg-stone-200 py-20 w-10/12 lg:w-8/12 mx-auto rounded-3xl  font-bold"> <h2 className=" text-2xl lg:text-4xl">No Class has Selected Yet</h2>
+           <p className="font-normal mt-2 text-gray-600">Please Visit All Classes to Select a class</p>
+           </div>
+        }
 
         </div>
     );
